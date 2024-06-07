3ad3941d2fd22031da270e7ef5f3c79444fdee53
davik20 <65835906+Meka-tech@users.noreply.github.com>
2024-06-07 11:50:54 +0100

    *

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const termRoutes = require("./routes/term");
 const detailRoutes = require("./routes/financeDetail");
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT,PATCH ,DELETE , OPTIONS"
